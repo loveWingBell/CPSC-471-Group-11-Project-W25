8e5c39a04892182f148bee05ef6e4da4fcf130de
yuinikoru.futamata <yuinikoru.futamata@ucalgary.ca>
2025-04-22 10:55:18 -0600

    configured mysql backend database server

--- medical_clinic/medical_clinic/settings.py
+++ medical_clinic/medical_clinic/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "medical_clinic.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CPSC471',
+        'USER': 'root',
+        'PASSWORD': 'vpn123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
